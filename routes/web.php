4f57cd63d0fd5c71eae4c3ebe121becea5b4682f
Max Griffon <mgriffon99@missouriwestern.edu>
2017-05-05 10:24:06 -0500

    commit 6

--- routes/web.php
+++ routes/web.php
@@ -34,17 +34,34 @@ Route::group(['prefix' => 'RB'], function () {
         $queryx = "SELECT TOTAL_YARDS FROM RB WHERE JERSEY_NUMBER = $x";
         $queryy = "SELECT TOTAL_YARDS FROM RB WHERE JERSEY_NUMBER = $y";
 
-        $RB1 = \DB::where('kdumlerfootball')->select($queryx);
-        $RB2 = \DB::where('kdumlerfootball')->select($queryy);
+        $RB1 = \DB::connection('kdumlerfootball')->select($queryx);
+        $RB2 = \DB::connection('kdumlerfootball')->select($queryy);
 
-        if ($RB1 > $RB2) {
-            $result = $RB1 - $RB2;
+        $rbYards1 = $RB1[0]->TOTAL_YARDS;
+        $rbYards2 = $RB2[0]->TOTAL_YARDS;
+
+        if ($rbYards1 > $rbYards2) {
+            $result = $rbYards1 - $rbYards2;
         } else {
-            $result = $RB2 - $RB1;
+            $result = $rbYards2 - $rbYards1;
         }
 
-        return "<h1> $RB1 had $result more yards than $RB2";
-    })->where(['x' => '[0-9]?', 'y' => '[0-9]?']);
+        if($rbYards1 > $rbYards2){
+            $number1 = $x;
+        } else {
+            $number1 = $y;
+        }
+
+        if($rbYards1 < $rbYards2){
+            $number2 = $x;
+        } else {
+            $number2 = $y;
+        }
+
+
+        return "<h1> Number $number1 had $result more rushing yards than number $number2 ";
+
+    })->where(['x' => '[0-9]|[1-9][0-9]', 'y' => '[0-9]|[1-9][0-9]']);
 
 
 });
@@ -54,17 +71,37 @@ Route::group(['prefix' => 'LB'], function () {
 
     Route::get('/stats', 'positions@LB');
     Route::get('/compare/{x}/{y}', function ($x, $y) {                  //NEEDS TO CALCULATE
-        $LB1 = DataBase::LB('TOTAL_YARDS')->where('NAME', "$x");
-        $LB2 = DataBase::LB('TOTAL_YARDS')->where('NAME', "$y");
+        $queryx = "SELECT TOTAL_TACKLES FROM LB WHERE JERSEY_NUMBER = $x";
+        $queryy = "SELECT TOTAL_TACKLES FROM LB WHERE JERSEY_NUMBER = $y";
 
-        if ($LB1 > $LB2) {
-            $result = $LB1 - $LB2;
+        $LB1 = \DB::connection('kdumlerfootball')->select($queryx);
+        $LB2 = \DB::connection('kdumlerfootball')->select($queryy);
+
+        $lbTackles1 = $LB1[0]->TOTAL_TACKLES;
+        $lbTackles2 = $LB2[0]->TOTAL_TACKLES;
+
+        if ($lbTackles1 > $lbTackles2) {
+            $result = $lbTackles1 - $lbTackles2;
         } else {
-            $result = $LB2 - $LB1;
+            $result = $lbTackles2 - $lbTackles1;
         }
 
-        return "<h1> $x had $result more yards than $y";
-    })->where(['x' => '[A-Z]', 'x' => '[A-Z]']);
+        if($lbTackles1 > $lbTackles2){
+            $number1 = $x;
+        } else {
+            $number1 = $y;
+        }
+
+        if($lbTackles1 < $lbTackles2){
+            $number2 = $x;
+        } else {
+            $number2 = $y;
+        }
+
+
+        return "<h1> Number $number1 had $result more tackles than number $number2 ";
+
+    })->where(['x' => '[0-9]|[1-9][0-9]', 'y' => '[0-9]|[1-9][0-9]']);
 
 });
 
@@ -72,15 +109,35 @@ Route::group(['prefix' => 'WR'], function () {
 
     Route::get('/stats', 'positions@WR');
     Route::get('/compare/{x}/{y}', function ($x, $y) {                  //NEEDS TO CALCULATE
-        $WR1 = DataBase::WR('TOTAL_YARDS')->where('NAME', "$x");
-        $WR2 = DataBase::WR('TOTAL_YARDS')->where('NAME', "$y");
+        $queryx = "SELECT TOTAL_YARDS FROM WR WHERE JERSEY_NUMBER = $x";
+        $queryy = "SELECT TOTAL_YARDS FROM WR WHERE JERSEY_NUMBER = $y";
 
-        if ($WR1 > $WR2) {
-            $result = $WR1 - $WR2;
+        $WR1 = \DB::connection('kdumlerfootball')->select($queryx);
+        $WR2 = \DB::connection('kdumlerfootball')->select($queryy);
+
+        $wrYards1 = $WR1[0]->TOTAL_YARDS;
+        $wrYards2 = $WR2[0]->TOTAL_YARDS;
+
+        if ($wrYards1 > $wrYards2) {
+            $result = $wrYards1 - $wrYards2;
         } else {
-            $result = $WR2 - $WR1;
+            $result = $wrYards2 - $wrYards1;
         }
 
-        return "<h1>$x  had $result more yards than $y";
-    })->where(['x' => '[A-Z]', 'x' => '[A-Z]']);
+        if($wrYards1 > $wrYards2){
+            $number1 = $x;
+        } else {
+            $number1 = $y;
+        }
+
+        if($wrYards1 < $wrYards2){
+            $number2 = $x;
+        } else {
+            $number2 = $y;
+        }
+
+
+        return "<h1> Number $number1 had $result more recieving yards than number $number2 ";
+
+    })->where(['x' => '[0-9]|[1-9][0-9]', 'y' => '[0-9]|[1-9][0-9]']);
 });
